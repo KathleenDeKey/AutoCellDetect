e0f7e5e32e4cfece05c56872baaf7271d0989802
KathleenDeKey <145138248+KathleenDeKey@users.noreply.github.com>
2023-11-11 18:08:13 -0500

    Add files via upload

--- CellDetection6.py
+++ CellDetection6.py
@@ -62,42 +62,41 @@ def store_values(detected, trimage):
                 index += 1
     return cells
 
-def find_intensity(detected, trimage):
-    # Calculate the average pixel intensity
-    gray = cv2.cvtColor(trimage, cv2.COLOR_BGR2GRAY)
-    global_average_intensity = np.mean(gray)
-    print(f'Average pixel intensity of the entire image: {global_average_intensity}')
+def find_intensity(center, radius, trimage):
+    (x, y) = center
+    # Create an empty mask image with the same size as the input image
+    # mask = np.zeros(trimage.shape[:2], dtype=np.uint8)
+    total_intensity = 0;
+    numpixel = 0
+    # Traverse through each pixel and check if it's within the circle
+    for i in range(trimage.shape[0]):
+        for j in range(trimage.shape[1]):
+            # Calculate the distance from the center (x, y)
+            distance = np.sqrt((j - x) ** 2 + (i - y) ** 2)
+            # If the distance is less than or equal to the radius, the pixel is inside the circle
+            if distance <= radius:
+                # Mark this pixel as part of the circle in the mask
+                intensity = trimage[y, x]
+                total_intensity += intensity
+                numpixel += 1;
+    # Use the mask to extract the pixels within the circle from the original image
+    # cv2.imshow('mask', mask)
+    # circle_pixels = cv2.bitwise_and(trimage, trimage, mask=mask)
+    # cv2.imshow('circle_pixels',circle_pixels)
+    # cv2.waitKey(0)
+    # Calculate the average intensity of the pixels within the circle
+    average_intensity = total_intensity // numpixel
+    print(average_intensity)
+    # corrected total cell fluorescence (CTCF) = Integrated Density – (Area of Selected Cell x Mean Fluorescence of Background readings)
+    # CTCF = average_intensity - (np.pi * (radius ** 2) * global_average_intensity)
+    # print(CTCF)
 
-    # traverse through each circle to process intensity
-    if detected is not None:
-        circles = np.uint16(detected)
-        for circle in circles[0, :]:
-            x, y = (circle[0], circle[1])
-            radius = circle[2]
-            # Create an empty mask image with the same size as the input image
-            mask = np.zeros(trimage.shape[:2], dtype=np.uint8)
-            # Traverse through each pixel and check if it's within the circle
-            for i in range(trimage.shape[0]):
-                for j in range(trimage.shape[1]):
-                    # Calculate the distance from the center (x, y)
-                    distance = np.sqrt((j - x) ** 2 + (i - y) ** 2)
-                    # If the distance is less than or equal to the radius, the pixel is inside the circle
-                    if distance <= radius:
-                        # Mark this pixel as part of the circle in the mask
-                        mask[i, j] = 255
-            # Use the mask to extract the pixels within the circle from the original image
-            circle_pixels = cv2.bitwise_and(trimage, trimage, mask=mask)
-            # Calculate the average intensity of the pixels within the circle
-            average_intensity = np.mean(circle_pixels)
-            # corrected total cell fluorescence (CTCF) = Integrated Density – (Area of Selected Cell x Mean Fluorescence of Background readings)
-            # CTCF =
-    return average_intensity
 
 # Create trackbars
 cv2.createTrackbar("Max Radius", "Enter Values", 0, 20, on_trackbar)
 cv2.createTrackbar("Min Radius", "Enter Values", 0, 20, on_trackbar)
 cv2.createTrackbar("Min Distance", "Enter Values", 0, 20, on_trackbar)
-cv2.createTrackbar("Accumulator Resolution", "Enter Values", 0, 10, on_trackbar)
+cv2.createTrackbar("Accumulator Resolution", "Enter Values", 0, 100, on_trackbar)
 cv2.createTrackbar("Edge Threshold", "Enter Values", 0, 10, on_trackbar)
 cv2.createTrackbar("Accumulator Threshold", "Enter Values", 0, 20, on_trackbar)
 cv2.createTrackbar("Contrast", "Enter Values", 0, 20, on_trackbar)
@@ -106,17 +105,17 @@ cv2.setTrackbarMin("Brightness", "Enter Values", -127)
 
 while True:
     # load images
-    bfimg = cv2.imread('bf 6.tif', cv2.IMREAD_GRAYSCALE)
+    bfimg = cv2.imread('intensityTest.tif', cv2.IMREAD_GRAYSCALE)
     # bfimgGray = cv2.equalizeHist(bfimg)
-    trimg = cv2.imread('tr 6.tif')
+    trimg = cv2.imread('intensityTest.tif')
 
     # sets values
     minRad = cv2.getTrackbarPos("Min Radius", "Enter Values")
     maxRad = cv2.getTrackbarPos("Max Radius", "Enter Values")
     minDist = cv2.getTrackbarPos("Min Distance", "Enter Values")
-    dp = cv2.getTrackbarPos("Accumulator Resolution", "Enter Values") / 10.0
-    edge_threshold = cv2.getTrackbarPos("Edge Threshold", "Enter Values")
-    accumulator_threshold = cv2.getTrackbarPos("Accumulator Threshold", "Enter Values") /10.0
+    dp = cv2.getTrackbarPos("Accumulator Resolution", "Enter Values") / 100.0
+    edge_threshold = cv2.getTrackbarPos("Edge Threshold", "Enter Values") / 10.0
+    accumulator_threshold = cv2.getTrackbarPos("Accumulator Threshold", "Enter Values")
     alpha = cv2.getTrackbarPos("Contrast", "Enter Values") / 10.0  # Contrast control (1.0 means no change)
     beta = cv2.getTrackbarPos("Brightness", "Enter Values")  # Brightness control (0 means no change)
 
@@ -138,6 +137,7 @@ while True:
         break
     # press 'w' to write processed file
     if key == ord('w'):
+        print("writing file")
         cells = store_values(detected_circles, trimg)
         for key, value in cells.values():
             print(key + " : " + cells[key]['Intensity']);
